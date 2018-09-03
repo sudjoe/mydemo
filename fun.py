0fe4d4af029938d58da3827a9681d55a2abc9d57
Giridhar Kadam <giridharkadam@gmail.com>
2018-09-03 13:08:18 +0530

    all files

---
+++ fun.py
@@ -0,0 +1,26 @@
+def myfun(a,b,c,name,data,add):
+    '''
+
+    :param a: for integer value
+    :return: None
+    '''
+    print"myfuction",a,b,c
+    print"myfuction", name,data,add
+
+myfun(1,3,4,name=1,data=2,add=34)
+
+def myfun2(*s,**ss):
+    '''
+
+    :param a: for integer value
+    :return: None
+    '''
+    print"myfuction",s
+    print"myfuction", ss
+
+myfun2(1,3,4,name=1,data=2,add=34)
+
+
+
+
+
