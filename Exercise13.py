4ff70f377bada8c0b9fc7b28c0483450921f05e5
W2T001 <w2t@gmail.com>
2018-09-04 16:32:58 +0530

    Thirteenth PrograM

--- Exercise13.py
+++ Exercise13.py
@@ -1,6 +1,7 @@
 from sys import argv
-script, first, second, third, = argv
-print "The script is called:", script
-print "Your first variable is:", first
-print "Your second variable is:", second
-print "Your third variable is:", third
+script, first, second, third, = argv      # (while executing the prog .py give parameters)
+print "The script is called:", script      #1st is input file name
+print "Your first variable is:", first     #2nd 
+print "Your second variable is:", second   #3rd
+print "Your third variable is:", third     #4th 
+ # give 3 values after including the file name as first value. while running the file.
