a23cfb757c4ba431cf0abdc7931f6eeba7755a9b
Giridhar Kadam <giridharkadam@gmail.com>
2018-09-10 16:29:53 +0530

    18th Design Pattern

--- MVC.py
+++ MVC.py
@@ -21,3 +21,39 @@ class Person(object):
             person = Person(item['first_name'], item['last_name'])
             result.append(person)
         return result
+
+  #
+    from model import Person
+    def showAllView(list):
+        print 'In our db we have %i users. Here they are:' % len(list)
+        for item in list:
+            print item.name()
+
+    def startView():
+        print 'MVC - the simplest example'
+        print 'Do you want to see everyone in my db?[y/n]'
+
+    def endView():
+        print 'Goodbye!'
+
+     #
+        from model import Person
+        import view
+
+        def showAll():
+            # gets list of all Person objects
+            people_in_db = Person.getAll()
+            # calls view
+            return view.showAllView(people_in_db)
+
+        def start():
+            view.startView()
+            input = raw_input()
+            if input == 'y':
+                return showAll()
+            else:
+                return view.endView()
+
+        if __name__ == "__main__":
+            # running controller function
+            start()
