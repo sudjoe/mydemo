1e19ae8937cbdb9eceb0a2ae3fabe217870dafef
Giridhar Kadam <giridharkadam@gmail.com>
2018-09-12 11:02:25 +0530

    INIT Program

---
+++ INIT.py
@@ -0,0 +1,14 @@
+# A Sample class with init method
+class Person:
+    # init method or constructor
+    def __init__(self, name):
+        self.name = name
+
+        # Sample Method
+
+    def say_hi(self):
+        print('Hello, my name is', self.name)
+
+
+p = Person('SUDHAKAR SINGH')
+p.say_hi()
