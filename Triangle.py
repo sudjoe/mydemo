4ea76890fe461b8e915d23219f454659c560bd3d
Giridhar Kadam <giridharkadam@gmail.com>
2018-09-17 11:44:27 +0530

    Javatpoint Program

---
+++ Triangle.py
@@ -0,0 +1,12 @@
+# Three sides of the triangle is a, b and c:
+a = float(input('Enter first side: '))
+b = float(input('Enter second side: '))
+c = float(input('Enter third side: '))
+
+# calculate the semi-perimeter
+s = (a + b + c) / 2
+print('The semi-perimeter of Traingle is {}'.format(s))
+
+# calculate the area
+area = (s * (s - a) * (s - b) * (s - c)) ** 0.5
+print('The area of the triangle is %0.2f' % area)
